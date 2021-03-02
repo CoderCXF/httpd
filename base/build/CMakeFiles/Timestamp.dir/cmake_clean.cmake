a54cd24def104063da920d8166b4e41ac11d4a5d
CoderCXF <18251716983@163.com>
2021-03-02 19:46:38 +0800

    commit

---
+++ base/build/CMakeFiles/Timestamp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Timestamp.dir/Timestamp.cc.o"
+  "CMakeFiles/Timestamp.dir/test/Timestamp_test.cpp.o"
+  "bin/Timestamp"
+  "bin/Timestamp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Timestamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ net/poller/PollPoller.h
@@ -0,0 +1,57 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-02 17:11:33
+ * @LastEditTime: 2021-03-02 17:20:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /WebServer/net/poller/PollPoller.h
+ */
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/muduo/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+//
+// This is an internal header file, you should not include this.
+
+#ifndef POLLPOLLER_H
+#define POLLPOLLER_H
+
+#include "muduo/net/Poller.h"
+
+#include <vector>
+
+struct pollfd;
+
+namespace muduo
+{
+namespace net
+{
+
+///
+/// IO Multiplexing with poll(2).
+///
+class PollPoller : public Poller
+{
+ public:
+
+  PollPoller(EventLoop* loop);
+  ~PollPoller() override;
+
+  Timestamp poll(int timeoutMs, ChannelList* activeChannels) override;
+  void updateChannel(Channel* channel) override;
+  void removeChannel(Channel* channel) override;
+
+ private:
+  void fillActiveChannels(int numEvents,
+                          ChannelList* activeChannels) const;
+
+  typedef std::vector<struct pollfd> PollFdList;
+  PollFdList pollfds_;
+};
+
+}  // namespace net
+}  // namespace muduo
+#endif  // MUDUO_NET_POLLER_POLLPOLLER_H
